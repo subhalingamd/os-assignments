eea6ac56d01bf7e77d0514fcfe901f83f6442865
Subhalingam D <subhalingam.d@gmail.com>
2021-03-07 23:27:25 +0530

    doc?? checkkkk

--- assg1/shell.c
+++ assg1/shell.c
@@ -1,3 +1,8 @@
+/****************************************************************
+Program:	A simple shell in C
+Author: 	Subhalingam D
+****************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> 
@@ -6,32 +11,38 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define MAX_ARGS  16
-#define MAX_ARGL  1024
-#define MAX_CHARS 1024
-#define MAX_PATHL 1024
-#define MAX_HIST  5
+#define MAX_ARGS  16		// max space seperated arguments (words) in command
+#define MAX_ARGL  1024		// max length of an argument in command
+#define MAX_CHARS 1024		// max num of characters in the input command
+#define MAX_PATHL 1024		// max length of absolute path of starting directory
+#define MAX_HIST  5			// max number of inputs to keep a track of (size of history)
 
 
 int main(int argc, char* argv[]) 
 {
+	// variables and local functions declarations
 	void parse_input(char*,char*[],char*), update_history(char*,char*[],int*,int*,int*), serve_history(char*[],int,int,int);
-	char *path_resolver(char*,char*,char[],int), home_path[MAX_PATHL], *_home_path = getcwd(home_path,sizeof(home_path)), cwd[MAX_PATHL+MAX_CHARS+2], *curr_path=strdup("~"), *history[MAX_HIST];
+	char *path_resolver(char*,char*,char[],int), home_path[MAX_PATHL], *_home_path = getcwd(home_path,sizeof(home_path)), cwd[MAX_PATHL+MAX_CHARS+2], *curr_path=strdup("~"), *history[MAX_HIST]; // (+2) used in size to account for the extra '\n' and '\0' in string input
 	int get_input(char*), history_start = -1, history_count = 0, history_global_count = 0;
 
+	// allocate memory for history and initialise with '\0'
 	for(int i=0;i<MAX_HIST;i++) {history[i]=(char*)calloc(MAX_CHARS+2,sizeof(char));}
+	
 	while (1){
+		// declare variables for user input and args for execvp() and initialise them
 		char *cargs[MAX_ARGS]; for(int i=0;i<MAX_ARGS;i++){cargs[i]=(char*)calloc(MAX_PATHL+MAX_ARGL+2,sizeof(char));}
 		char *inp=(char*)calloc(MAX_CHARS+2,sizeof(char));
 
+		// curr_path stores the current working directory path to display on shell
 		printf("\033[32;1mMTL458:%s$ \033[0m",curr_path);
 
-		if (!get_input(inp)) continue;	// skip if empty input/limit exceeded
-		parse_input(inp,cargs,_home_path);
+		if (!get_input(inp)) continue;		// skip if empty input/limit exceeded
+		parse_input(inp,cargs,_home_path);	// parse the given input to prepare for execvp()
 		
 		update_history(inp,history,&history_start,&history_count,&history_global_count); // this should come before any command handling!!
 		if (cargs[0]==NULL) {continue;}  // handle empty line as input except "\n" (eg. "  \n")
 
+		// serve request for cd
 		if (!strcmp(cargs[0],"cd")) { if(cargs[1]) curr_path=path_resolver(cargs[1],_home_path,cwd,sizeof(cwd)); continue;}
 
 		int pid = fork();
@@ -61,7 +72,15 @@ int main(int argc, char* argv[])
 }
 
 int get_input(char *inp){
-
+	/*
+	Description:	Get input from stdin
+	Input:
+		- inp: stores the input
+	Returns:
+		- Returns 1 if the input is valid or 0 otherwise
+	Notes:
+		- An input is considered invalid/unnecessary if it is empty, has got only '\n' or the input length exceeded the maximum limit (excluding the '\n' and '\0') as defined in MAX_CHARS
+	*/
 	if (fgets(inp,MAX_CHARS+2,stdin)){
 		size_t len = strlen(inp); 
 		if (len == 0){	// empty input
@@ -85,6 +104,16 @@ int get_input(char *inp){
 }
 
 void parse_input(char *inp, char *cargs[], char *start){
+	/*
+	Description:	Parse the user input to serve it to execvp()
+	Input:
+		- inp: the user input
+		- cargs: the array of strings in the format expected by execvp()
+		- start: the path of the directory from where the shell was started (also referred to as HOME)
+	Returns:
+	Notes:
+	*/
+
 	// for(;*inp==' ';inp++);  // strip leading whitespace...
 	// if (!*inp) {cargs[0]=NULL; return;} // handling empty line case seperately
 
@@ -106,19 +135,32 @@ void parse_input(char *inp, char *cargs[], char *start){
 			else {
 				if (c==0 && *inp=='~') {	// starts with ~ 
 					char *t = start;
-					while((cargs[w][c++]=*t++));
-					c--; inp++;
+					while((cargs[w][c++]=*t++));	// replace ~ with HOME (starting directory path)
+					c--; inp++;		// the last character copied from start would be '\0', so go back one step (c--) <and> go to next char in input (inp++)
 				}
-				else cargs[w][c++]=*inp++;	
+				else cargs[w][c++]=*inp++;	// just copy the character otherwise
 			}
 		}
 
 	}
-	if (cargs[w][0] == '\0') cargs[w]=NULL; else cargs[++w] = NULL;  // entry/exit with empty string test
+	if (cargs[w][0] == '\0') cargs[w]=NULL; else cargs[++w] = NULL;  // execvp() expects the last entry to be NULL
 
 }
 
 char* path_resolver(char *path,char *start, char buff[], int size_buff){
+	/*
+	Description:	Serve cd command
+	Input:
+		- inp: the user input
+		- start: the path of the directory from where the shell was started (also referred to as HOME)
+		- buff: initially empty, contains the path to be displayed on shell
+		- size_buff: size allocated for the buffer
+	Returns:
+		- a pointer to buff
+	Notes:
+		- along with changing the current working directory (cwd), the path that has to be displayed in the shell (with changes to ~) is also modified accordingly
+	*/
+
 	// if (!strcmp(start,"/")) start= (char*) ""; // ?? handle start with "/"
 	/****** handled in parse_input()
 	char t[MAX_PATHL+MAX_CHARS+2];
@@ -128,20 +170,41 @@ char* path_resolver(char *path,char *start, char buff[], int size_buff){
 	}
 	path=t;
 	*********/
+
 	if (chdir(path)) { fprintf(stderr,"cd: %s: ",path); perror(""); }
 	char *cwd = getcwd(buff,size_buff);
-	// if (!*cwd) { } // handle this later...
 
 	while (*start&&*cwd&&*start==*cwd){ start++; cwd++; }
+		// breaks if: \0 encountered in start or cwd or cwd!=start..
+		// if start has ended (ie \0 reached) and [ cwd has also reached end (\0) (=> ~) or cwd has / (=> ~/...) ]
+		// if start has not ended (=> out of ~) or start has ended but cwd has some char other than \0 and / (=> starting is okay... but the ending folder name in cwd is prefix of last dir in start --> so not under ~) ==> show the abs path
 	if (!*start&&(!*cwd||*cwd=='/')) { *--cwd='~'; return cwd; } // handle???
 	return buff;
 }
 
 void update_history(char* inp,char *history[],int* start,int* count,int* tot_count){
+	/*
+	Description:	Update history for each valid input (inc history command)
+	Input:
+		- inp: the user input
+		- history: array that stores the history
+		- start: the position at which the previous command was inserted in the history array
+		- count: max(the number of commands entered till now, MAX_HIST) - used for serving history
+		- tot_count: total number of inputs by user (this number is used to display the serial number of the previous inputs when history command is used)
+	Returns:
+	Notes:
+		- the history array is implemented in a circular fashion to optimise the space consumed
+			- the size of the array always remains as MAX_HIST
+			- the new input is inserted at (prev_index+1)%MAX_HIST starting from 0
+			- so the inputs are inserted in the order -> 0,1,2,3,...,MAX_HIST-1,0,1,2,3,...
+		- the start, count, tot_count are passed by reference
+	*/
+
 	// you can't get history w/o typing history...
 	++*start; *start%=MAX_HIST; // if count<MAX_HIST, start<MAX_HIST
 	strcpy(history[*start],inp); // copy each char in inp to history
 	
+	/// same thing below was achieved using strcpy()
 	//int i = 0;
 	//while((history[*start][i++] = *inp++)); // copy each char in inp to history
 	
@@ -151,6 +214,21 @@ void update_history(char* inp,char *history[],int* start,int* count,int* tot_cou
 }
 
 void serve_history(char *history[], int start, int count, int tot_count){
+	/*
+	Description:	Print history when requested
+	Input:
+		- history: array that stores the history
+		- start: the position at which the most recent command was inserted in the history array
+		- count: max(the number of commands entered till now, MAX_HIST) - used for getting the indices of the history array
+		- tot_count: total number of inputs by user (this number is used to display the serial number of the previous inputs)
+	Returns:
+	Notes:
+		- the history array is implemented in a circular fashion to optimise the space consumed (as mentioned in update_history())
+			- the most recent input will be stored in start
+			- (start+1)%count will have the desired oldest input (MAX_HIST-th from bottom)
+			- the order indices for listing the history will be -> (start+1)%count, (start+2)%count, ..., start
+			- the serial number for the input can be computed accordingly
+	*/
 	for (int i=0; i<count; i++){
 		printf("%5d  %s\n",tot_count-count+i+1,history[(start+i+1)%count]);
 	}
